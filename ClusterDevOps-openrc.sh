88ca0050c280dc2d0abc62cdaf96b7aa14511501
Joshua McKenty <jmckenty@gmail.com>
2013-11-19 15:57:11 -0800

    Basic farmboy kickoff for demo devcloud

---
+++ ClusterDevOps-openrc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# With the addition of Keystone, to use an openstack cloud you should
+# authenticate against keystone, which returns a **Token** and **Service
+# Catalog**.  The catalog contains the endpoint for all services the
+# user/tenant has access to - including nova, glance, keystone, swift.
+#
+# *NOTE*: Using the 2.0 *auth api* does not mean that compute api is 2.0.  We
+# will use the 1.1 *compute api*
+export OS_AUTH_URL=http://205.234.30.227:5000/v2.0
+
+# With the addition of Keystone we have standardized on the term **tenant**
+# as the entity that owns the resources.
+export OS_TENANT_ID=a0c4d8db9f86497ab8a459ad205c1c64
+export OS_TENANT_NAME="ClusterDevOps"
+
+# In addition to the owning entity (tenant), openstack stores the entity
+# performing the action as the **user**.
+export OS_USERNAME=admin
+
+# With Keystone you pass the keystone password.
+echo "Please enter your OpenStack Password: "
+read -s OS_PASSWORD_INPUT
+export OS_PASSWORD=$OS_PASSWORD_INPUT
